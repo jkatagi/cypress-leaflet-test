4fe82833c1d11ceb30441df27cc56fe12db05d7b
jkatagi <kemisknkn37@gmail.com>
2018-09-13 00:49:11 +0900

    change mouse

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -5,13 +5,8 @@ describe('cypress leaflet test', () => {
 
     // move pin
     cy.get('.leaflet-marker-draggable')
-      .trigger('mousedown')
-      .trigger('dragstart', {which: 1, force: true})
-      .trigger('drag', {which: 1, x: 200, y: 100, force: true} )
-      .trigger('dragend', {which: 1, x: 200, y: 100, force: true})
-      .trigger('mouseup', {which: 1, x: 200, y: 100, force: true})
-//      .trigger('mousedown')
-//      .trigger('mousemove', {x: 100, y: 100, force: true})
-//      .trigger('mousup', {x: 100, y: 100, force: true})
+      .trigger('mousedown', {which: 1, force: true})
+      .trigger('mousemove', {x: 100, y: 100, force: true})
+      .trigger('mouseup', {x: 100, y: 100, force: true})
   })
 })
